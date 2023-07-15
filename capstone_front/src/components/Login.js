ce73a0c10270fb6c120d5f32c3a9c3a580c2359c
Nichol Lecera <nichollecera11@gmail.com>
2023-07-15 14:42:06 +0800

    push front

--- capstone_front/src/components/Login.js
+++ capstone_front/src/components/Login.js
@@ -1,25 +1,84 @@
-import { Button, FormControl, TextField } from "@mui/material";
-import React from "react";
-import './Logincss.css'
-import Logo from './capstoneLogo.png'
+import { Alert, Button, FormControl, TextField } from "@mui/material";
+import React, { useState } from "react";
+import "./Logincss.css";
+import Logo from "./capstoneLogo.png";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigation = useNavigate();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-    const buttonClick = (operation) => {
+  const [error, setError] = useState("");
+  const [submitLoading, setsubmitLoading] = useState(false);
 
-        if (operation === "login") { }
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    setError("");
+    setsubmitLoading(true);
+
+    if (!validateForm()) {
+      setsubmitLoading(false);
+      return;
+    } else {
+      console.log("logging in");
+      try {
+        const response = await axios.post(
+          "http://127.0.0.1:8000/api/login",
+          formData
+        );
+        const token = response.data.token;
+        localStorage.setItem("login_token", token);
+        console.log(response.data.token);
+        navigation("/home");
+      } catch (error) {
+        console.log(error);
+        let errorMessage = error.response.data.error;
+        setError(errorMessage);
+      }
+      setsubmitLoading(false);
     }
+  }
+
+  const buttonClick = async () => {
+    
+    
+  }
+
+  const validateForm = () => {
+    if (formData.email === undefined || formData.email === "") {
+      setError(`Email is required!`);
+      return false;
+    } else if (formData.password === undefined || formData.password === "") {
+      setError(`password is required!`);
+      return false;
+    }
+    return true;
+  };
+
+  const handleChange = (event) => {
+    // console.log(event.target.name, event.target.value);
+    setFormData((prevstate) => ({
+      ...prevstate,
+      [event.target.name]: event.target.value,
+    }));
+  };
 
   return (
     <div className="loginRoot">
-          <img src={Logo} alt='logo' style={{marginTop:"50px"}}/>
-      <FormControl className='formControl'>
+      <img src={Logo} alt="logo" style={{ marginTop: "50px" }} />
+      {error && <Alert severity="error">{error}</Alert>}
+      <FormControl className="formControl">
         <TextField
           label="email"
           name="email"
           variant="outlined"
           fullWidth
           required
+          onChange={handleChange}
         />
         <TextField
           label="password"
@@ -27,13 +86,14 @@ function Login() {
           variant="outlined"
           fullWidth
           required
+          onChange={handleChange}
         />
         <Button
           className="loginButton"
           variant="contained"
           color="primary"
           fullWidth
-          onClick={() => buttonClick("login")}
+          onClick={handleLogin}
         >
           Login
         </Button>
@@ -42,7 +102,7 @@ function Login() {
           variant="contained"
           color="primary"
           fullWidth
-          onClick={() => buttonClick("login")}
+          onClick={buttonClick}
         >
           Forgot Password
         </Button>
